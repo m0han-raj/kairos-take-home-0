cc65418346ae66f167a5d63a305493d3423afbf2
devs-mohanraj <mohan.ins.nvm@gmail.com>
2025-06-25 23:55:22 +0530

    updated agent

--- agent_host/agent.py
+++ agent_host/agent.py
@@ -1,14 +1,11 @@
 from mcp_servers.search_paper import search_for_paper
 from mcp_servers.summarize import summarize_content 
-from utils.logger import log_tool
 from datetime import datetime
 
 def run_agent(query):
     timestamp = datetime.now().isoformat()
     papers = search_for_paper(query)
-    log_tool("search_for_paper", {"query": query}, papers, timestamp)
 
     for paper in papers:
-        summary = summarize_content(paper["pdf_url"])
-        log_tool("summarize_content", {"pdf_url": paper["pdf_url"]}, summary, timestamp)
+        summary = summarize_content(papers[0]["pdf_link"])
         print(f"Paper: {paper['title']}\nSummary: {summary['summary']}\n")
